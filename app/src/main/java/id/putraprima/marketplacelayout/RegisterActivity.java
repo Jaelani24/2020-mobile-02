85c92e36402826ca24f5d2bbf27c756580d4b391
Jaelani24 <jthebluess@gmail.com>
2024-03-31 20:06:30 +0700

    Update sampai Halaman Registrasi

--- app/src/main/java/id/putraprima/marketplacelayout/RegisterActivity.java
+++ app/src/main/java/id/putraprima/marketplacelayout/RegisterActivity.java
@@ -6,17 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 
-    public void clickLogin(View view) {
+    public void clickBack(View view) {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
+
+    public void clickNext(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 
 }
