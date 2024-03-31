e8d0d53b86ba85dc7e5f7a899c784192c1341cac
Jaelani24 <jthebluess@gmail.com>
2024-03-31 21:50:04 +0700

    Update Halaman Profile

--- app/src/main/java/id/putraprima/marketplacelayout/ProfileActivity.java
+++ app/src/main/java/id/putraprima/marketplacelayout/ProfileActivity.java
@@ -6,23 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class RegisterActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_profile);
     }
 
     public void clickBack(View view) {
-        Intent intent = new Intent(this, LoginActivity.class);
+        Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
     }
-
-    public void clickNext(View view) {
-        Intent intent = new Intent(this, ProfileActivity.class);
-        startActivity(intent);
-    }
-
 
 }
